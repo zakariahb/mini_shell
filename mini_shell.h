edc265c3b41e0673bfce7168dd4420ea295a2c7b
Zakaria Laksyar <zalaksya@e1r8p7.1337.ma>
2025-03-09 11:52:36 +0000

    mini_shell

--- mini_shell.h
+++ mini_shell.h
@@ -6,7 +6,7 @@
 /*   By: zalaksya <zalaksya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 14:41:54 by zalaksya          #+#    #+#             */
-/*   Updated: 2025/03/09 07:52:35 by zalaksya         ###   ########.fr       */
+/*   Updated: 2025/03/09 11:52:29 by zalaksya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@
 #include "string.h"
 #include "stdlib.h"
 #include "readline/readline.h"
+
+typedef struct s_shell
+{
+	int pipe;
+	int penthesises;
+	int qoute;
+	
+} t_shell;
 
 char	**ft_split(char const *s, char c);
 
