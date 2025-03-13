0d3548b40c8c2c557dfbb59ec657111b01fb5aee
Zakaria Laksyar <zalaksya@e3r1p17.1337.ma>
2025-03-13 09:58:18 +0000

    mini_shell

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zalaksya <zalaksya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 14:49:50 by zalaksya          #+#    #+#             */
-/*   Updated: 2025/03/13 08:57:52 by zalaksya         ###   ########.fr       */
+/*   Updated: 2025/03/13 09:44:08 by zalaksya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,45 +41,8 @@ int	ft_strcmp(char *s1, char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-int find_qoute(char *str, t_shell *input)
-{
-	int	i;
-	int	j;
 
-	i = 0;
-	j = 0;
-	while(str[i])
-	{
-		if (str[i] == '|' && str[i + 1] == '|' && str[i + 2] != '|')
-		{
-			input->or += 1;
-			j++;
-			if (j == 2)
-				return (0);	
-		}
-		i++;
-	}
-	if (j == 0)
-		return (0);
-	return (1);
-	
-}
-int check_user_input(char **ar)
-{
-	int	i;
-	t_shell *input = NULL;
-	i = 0;
-	while(ar[i])
-	{
-		printf("ar[%d]	:%s\n", i, ar[i]);
-		if (find_qoute(ar[i], input))
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-void create_tokens(char *str)
+void tokenazation(char *str, t_shell *input)
 {
 	int	i;
 
@@ -89,19 +52,31 @@ void create_tokens(char *str)
 		while(str[i] && (str[i] == 32 || str[i] == '\t' || str[i] == '\n'))
 			i++;
 		if (str[i] == '|')
-			ft_strcpy()
+			create_tokens(PIPE, "|");
+		else if (str[i] == '>' && str[i + 1] == '>')
+			create_tokens(AEPPEND, ">>");
+		else if (str[i] == '<' && str[i + 1] == '<')
+			create_tokens(HERDOK, "<<");
+		else if (str[i] == '>')
+			create_tokens(R_OUT, ">");
+		else if (str[i] == '<')
+			create_tokens(R_IN, "<");
+		else
+			
+		
 		
 	}
 	
 }
 int main (void)
 {
+	t_shell *input = malloc(sizeof(t_shell));
 	char *line = NULL;
 	while ((line = readline("shell> ")))
 	{
 		add_history(line);
 	}
-	create_tokens(line); 
+	create_tokens(line, input); 
 	printf("line :%s\n",line);
 	// char **ar = ft_split(line ,'|');
 	// if (check_user_input(ar))
