b6b6e851a8784bc05b9f28aa4a3b99c6554b7902
Zakaria Laksyar <zalaksya@e2r7p8.1337.ma>
2025-04-19 13:33:03 +0100

    mini_shell

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,11 @@
 /*   By: zalaksya <zalaksya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 16:28:35 by zalaksya          #+#    #+#             */
-/*   Updated: 2025/04/18 15:12:13 by zalaksya         ###   ########.fr       */
+/*   Updated: 2025/04/19 13:31:44 by zalaksya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini_shell.h"
-
-static char	**ft_free(char **ft_split_res, int count)
-{
-	while (count--)
-	{
-		free(ft_split_res[count]);
-	}
-	free(ft_split_res);
-	return (NULL);
-}
-
-static size_t	ft_strlenword(char *s, char c)
-{
-	size_t	len;
-	int		i;
-
-	len = 0;
-	i = 0;
-	while (s[i] != c && s[i] != '\0')
-	{
-		len++; 
-		i++;
-	}
-	return (len);
-}
 
 int ft_find(char c)
 {
@@ -52,19 +27,48 @@ int ft_find(char c)
 	}
 	return (0);
 }
+static char	**ft_free(char **ft_split_res, int count)
+{
+	while (count--)
+	{
+		free(ft_split_res[count]);
+	}
+	free(ft_split_res);
+	return (NULL);
+}
+
+int ft_tz(char c)
+{
+	if (c == ' ' || c == '<' || c == '>' || c == '|')
+		return (0);
+	return (1);
+}
+static size_t	ft_strlenword(char *s)
+{
+	size_t	len;
+	int		i;
+
+	len = 0;
+	i = 0;
+	while (s[i] && ft_tz(s[i]))
+	{
+		len++; 
+		i++;
+	}
+	return (len);
+}
 static int	count_word(char const *str)
 {
-	int	i;
-	int	j;
-	int	k;
-	char *str;
-	str = " <>|";
+	int		i;
+	int		j;
+	int		k;
+
 	i = 0;
 	j = 0;
 	k = 0;
 	while (str[i])
 	{
-		if (ft_strcmp(str[i] == c))
+		if (!ft_tz(str[i]))
 		{
 			j++;
 			k = 0;
@@ -78,7 +82,7 @@ static int	count_word(char const *str)
 	}
 	return (j);
 }
-wdwqdwq
+
 static char	*ft_strndup(char const *str, size_t len)
 {
 	size_t	i;
@@ -112,20 +116,34 @@ char	**ft_split(char const *s)
 	index = 0;
 	while (s[i])
 	{
-		while (s[i])
+		if (s[i] && !ft_tz(s[i]))
 		{
-			if (ft_find(s[i]))
-				p[index] = ft_strndup(&s[i], ft_strlenword((char *)&s[i], c));
-		}
+			p[index] = ft_strndup(&s[i], 2);
+			printf("P[index] :%s\n", p[index]);
 			i++;
+		}
 		if (!s[i])
 			break ;
-		p[index] = ft_strndup(&s[i], ft_strlenword((char *)&s[i], c));
+		p[index] = ft_strndup(&s[i], ft_strlenword((char *)&s[i]));
 		if (!p[index])
 			return (ft_free(p, index));
-		i += ft_strlenword((char *)&s[i], c);
+		i += ft_strlenword((char *)&s[i]);
 		index++;
 	}
 	p[index] = NULL;
 	return (p);
+}
+int main ()
+{
+	int i = 0;
+	char n[] = "zakaria < the > best | ";
+	printf("str[i] :%zu\n", ft_strlenword(n));
+	char **s = ft_split(n);
+	while (s[i])
+	{
+		printf("str[i] :%s\n", s[i]);
+		i++;
+		/* code */
+	}
+	
 }
